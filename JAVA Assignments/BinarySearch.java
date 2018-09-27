4b5d027ff3ecbf0243ca25048be4ed992bb9bcb5
Raj-18110 <sunandan.raj18110@gmail.com>
2018-09-27 09:54:41 +0530

    Update BinarySearch.java

--- JAVA Assignments/BinarySearch.java
+++ JAVA Assignments/BinarySearch.java
@@ -2,25 +2,26 @@ import java.util.*;
 public class BinarySearch {
 	public static void SearchKey(int array[],int a,int b,int c){
 		int mid=(a+b)/2;
-		while(c<array[a]&&c>array[b]){
+		if(c<array[a] || c>array[b]){
 			System.out.println("Not Found");
-			break;
+			return;
 		}
 		if(c==array[mid]){
 			System.out.println("Key is found at position "+mid);
 		}
 		else if(c>array[mid]){
-			SearchKey(array,mid,b,c);
+			SearchKey(array,mid+1,b,c);
 		}
 		else{
-			SearchKey(array,a,mid,c);
+			SearchKey(array,a,mid-1,c);
 		}
 	}
 	public static void main(String[] args) {
 		int len,n;
 		int arr[]={2,54,574,245,885,656,58,145,65};
+		//Arrays.sort(arr);
 		len=arr.length;
-		n=65;
+		n=85;
 		Arrays.sort(arr);
 		SearchKey(arr,0,len-1,n);
 	}
